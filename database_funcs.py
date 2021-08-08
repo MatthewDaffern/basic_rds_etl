617c155cee747899a5f70af1408dd32e8875489b
matthew daffern <enjoyskating101@gmail.com>
2021-08-07 23:51:09 -0700

    moving onto DB funcs

---
+++ database_funcs.py
@@ -0,0 +1,4 @@
+def commit_rows(database, rows):
+    """commits the rows"""
+    for i in rows:
+        # TODO
--- upload_to_s3_once.py
+++ upload_to_s3_once.py
@@ -1,7 +1,9 @@
 import json
 import pathlib
 import boto3
-
+import os
+import glob
+import time
 
 def load_json(file_location):
     """I like creating json dictionaries that store all my configuration settings"""
@@ -25,11 +27,25 @@ def upload_files_in_folder(file_collection, bucket_name, file_collection_folder)
     return list(map(lambda x: s3.meta.upload_file(str(file_collection_folder + x), bucket_name, x), file_collection))
 
 
+def clear_the_folder(file_collection_folder):
+    files = glob.glob(file_collection_folder)
+    for f in files:
+        os.remove(f)
+
+
 def main(config_location):
     config = load_json(config_location)
     files = get_files_in_folder(config['folder_location'])
-    return print_to_log(upload_files_in_folder(files, config['bucket_name'], config['folder_location']))
+    result = print_to_log(upload_files_in_folder(files, config['bucket_name'], config['folder_location']))
+    clear_the_folder(config['folder_location'])
+    return result
+
+
+def looper(sleep_time, function):
+    time.sleep(sleep_time)
+    return function
 
 
 if __name__ == '__main__':
-    main('monitored_folder.json')
+    config = load_json('monitored_folder.json')
+    looper(config['time_to_sleep'], main(config))
